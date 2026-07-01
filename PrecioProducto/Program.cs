222db271dbbb2e23599a297b8ba041584ec250dd
Chuliton <baezmonsserrath@gmail.com>
2026-06-30 22:23:26 -0300

    Initial commit

---
+++ PrecioProducto/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrecioProducto
+{
+    /*Pedir al usuario el precio de un producto (valor positivo) y la forma de pagar
+     (efectivo o tarjeta) si la forma de pago es mediante tarjeta, pedir el numero de 
+    cuenta (inventado)*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el precio del producto");
+            int precio = 0;
+            while (precio <= 0) {
+                precio = int.Parse(Console.ReadLine());
+                if (precio <= 0)
+                Console.WriteLine("El precio de debe ser mayor a 0");
+            }
+            Console.WriteLine("Ingrese el medio de pago (Efectivo o Tarjeta)");
+            string formaPago = Console.ReadLine().ToLower();
+            if (formaPago == "tarjeta")
+            {
+                Console.WriteLine("Ingrese el numero de cuenta");
+                int numCuenta = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Transaccion exitosa");
+        }
+    }
+}
